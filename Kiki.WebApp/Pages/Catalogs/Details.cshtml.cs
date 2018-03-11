9b37cb78130655412c84f428d5093295dc083f28
Selmir Hajruli <selmir@hajruli.ch>
2018-03-11 16:02:40 +0100

    Added crud views for catalogs

---
+++ Kiki.WebApp/Pages/Catalogs/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Kiki.WebApp.Data;
+using Kiki.WebApp.Data.Models;
+
+namespace Kiki.WebApp.Pages.Catalogs
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Kiki.WebApp.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Kiki.WebApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Catalog Catalog { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Catalog = await _context.Catalogs.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Catalog == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
