088eb06d8d7f61a0e45b56db5d4784e5095a9edc
Selmir Hajruli <selmir@hajruli.ch>
2018-03-11 15:27:36 +0100

    Implemented business entities

---
+++ Kiki.WebApp/Data/Models/Catalog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kiki.WebApp.Data.Models
+{
+    public class Catalog
+    {
+        public Catalog()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PriceColumn { get; set; }
+        public string SizeColumn { get; set; }
+        public string ReferenceColumn { get; set; }
+        public string Info1Column { get; set; }
+        public string Info2Column { get; set; }
+        public string Info3Column { get; set; }
+        public int StartLineNumber { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public string SizeColumnRegex { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
