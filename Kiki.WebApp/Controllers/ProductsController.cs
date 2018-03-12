b94b9cf74b47e01817442956d294694e51b1ad6c
Selmir Hajruli <selmir@hajruli.ch>
2018-03-12 07:00:28 +0100

    Started excel reader implemntation

---
+++ Kiki.WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kiki.WebApp.Controllers
+{
+    using System.Collections.Immutable;
+    using System.IO;
+    using Data;
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Internal;
+    using OfficeOpenXml;
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Calculate()
+        {
+            var catalogs = _context.Catalogs.ToImmutableList();
+            var rules = _context.DiscountRules.ToImmutableList();
+            foreach (var catalog in catalogs)
+            {
+                var existingFile = new FileInfo(catalog.FilePath);
+                using (var package = new ExcelPackage(existingFile))
+                {
+                    // get the first worksheet in the workbook
+                    var worksheet = package.Workbook.Worksheets[catalog.SheetIndex];
+
+                    var start = worksheet.Dimension.Start;
+                    var end = worksheet.Dimension.End;
+                    for (int row = start.Row; row <= end.Row; row++)
+                    {
+                        var s = Product.StringToSize(worksheet.Cells[catalog.SizeColumn + row].GetValue<string>(), catalog.SizeFormat);
+                        _context.Products.Add(new Product
+                        {
+                            BasePrice = worksheet.Cells[catalog.PriceColumn + row].GetValue<decimal>(),
+                            Size = s,
+                            Reference = worksheet.Cells[catalog.ReferenceColumn + row].GetValue<string>(),
+                            Info1 = worksheet.Cells[catalog.Info1Column + row].GetValue<string>(),
+                            Info2 = worksheet.Cells[catalog.Info2Column + row].GetValue<string>(),
+                            Info3 = worksheet.Cells[catalog.Info3Column + row].GetValue<string>(),
+                            CatalogId = catalog.Id,
+                            FinalPrice = CalculateFinalPrice(rules, s, worksheet.Cells[catalog.PriceColumn + row].GetValue<decimal>(), catalog.DiscountPercentage)
+                        });
+                    }
+                }
+            }
+        }
+
+        public int CalculateFinalPrice(ImmutableList<DiscountRule> rules, int size, decimal price, decimal discount)
+        {
+            var finalPrice = price - (price / 100 * discount);
+            var margin = rules.FirstOrDefault(r => r.Size == size && r.FromPrice >= finalPrice && r.ToPrice <= finalPrice)?
+                            .Margin;
+            return margin != null ? Convert.ToInt32(finalPrice + margin.Value) : 0;
+        }
+    }
+}
--- Kiki.WebApp/Data/Models/Product.cs
+++ Kiki.WebApp/Data/Models/Product.cs
@@ -1,5 +1,8 @@
 ﻿namespace Kiki.WebApp.Data.Models
 {
+    using System;
+    using Microsoft.AspNetCore.Rewrite.Internal.ApacheModRewrite;
+
     public class Product
     {
         public int Id { get; set; }
@@ -13,5 +16,25 @@
         public string Info3 { get; set; }
         public int CatalogId { get; set; }
         public Catalog Catalog { get; set; }
+
+        public static int StringToSize(string text, SizeFormatEnum sizeFormat)
+        {
+            text = text.ToLower().TrimEnd();
+            switch (sizeFormat)
+            {
+                case SizeFormatEnum.Last2AlphaNumeric:
+                    text = text.Substring(text.Length - 2, 2);
+                    break;
+                case SizeFormatEnum.Rxx:
+                    text = text.Substring(text.IndexOf('r'), 2);
+                    break;
+                case SizeFormatEnum.Simple:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(sizeFormat), sizeFormat, null);
+            }
+            int.TryParse(text, out var size);
+            return size;
+        }
     }
 }
