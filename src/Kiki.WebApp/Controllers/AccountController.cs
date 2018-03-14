aa9c8c4adeab88417f7180fea819d15050327819
Hajruli Selmir <selmir.hajruli@trianon.ch>
2018-03-14 10:59:06 +0100

    Fixed rights management

--- src/Kiki.WebApp/Controllers/AccountController.cs
+++ src/Kiki.WebApp/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ namespace Kiki.WebApp.Controllers
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger _logger;
 
-        public AccountController(SignInManager<ApplicationUser> signInManager, ILogger logger)
+        public AccountController(SignInManager<ApplicationUser> signInManager, ILogger<AccountController> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
