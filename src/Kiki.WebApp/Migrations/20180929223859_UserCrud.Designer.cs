3cc31b100cdd9dd0de4a0562bda9a0d751e8e386
Selmir <selmir@hajruli.ch>
2018-09-30 16:04:13 +0200

    Added migrations for roles and garages prices

--- src/Kiki.WebApp/Migrations/20180929223859_UserCrud.Designer.cs
+++ src/Kiki.WebApp/Migrations/20180929223859_UserCrud.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Kiki.WebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180929223859_UserCrud")]
+    partial class UserCrud
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
