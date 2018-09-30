5d4e039b4dfe450a71b959e71b027d92bd853aad
Selmir <selmir@hajruli.ch>
2018-09-30 16:03:06 +0200

    Added UtilisateurViewModel

--- src/Kiki.WebApp/Data/Models/ApplicationUser.cs
+++ src/Kiki.WebApp/Data/Models/ApplicationUser.cs
@@ -1,8 +1,25 @@
 namespace Kiki.WebApp.Data.Models
 {
+    using Kiki.WebApp.ViewModels;
     using Microsoft.AspNetCore.Identity;
 
     public class ApplicationUser : IdentityUser
     {
+        public string Entreprise { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public Utilisateur ToUtilisateur()
+        {
+            return new Utilisateur
+            {
+                Id = Id,
+                Email = Email,
+                UserName = UserName,
+                Entreprise = Entreprise,
+                Nom = Nom,
+                Prenom = Prenom,
+            };
+        }
     }
 }
---
+++ src/Kiki.WebApp/ViewModels/Utilisateur.cs
@@ -0,0 +1,36 @@
+﻿using Kiki.WebApp.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kiki.WebApp.ViewModels
+{
+    public class Utilisateur
+    {
+        public string Id { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "Le mot de passe doit contenir au moins 8 caractères, dont au moins une miniscule, une majusucule, un chiffre et un caractère spécial.")]
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+        public string Entreprise { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public ApplicationUser ToApplicationUser()
+        {
+            return new ApplicationUser
+            {
+                Id = Id,
+                Email = Email,
+                UserName = Email,
+                Entreprise = Entreprise,
+                Nom = Nom,
+                Prenom = Prenom,
+            };
+        }
+    }
+}
